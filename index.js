17ec1748152abdf4c26db1e54bd6d12f163a0cf2
Satyam kumar <satyamkumar2716@gmail.com>
2024-07-02 20:47:45 +0530

    change cors

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
   // res.header(`Access-Control-Allow-Origin`, `*`);
   // res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
   // res.header(`Access-Control-Allow-Headers`, `Content-Type`);
-  next();
+  // next();
 });
 
 
@@ -95,7 +95,7 @@ app.post('/admin/login', (req, res) => {
 });
 
 // Handle OPTIONS requests (preflight requests)
-app.options('*', cors(corsOptions)); // Enable preflight across all routes
+// app.options('*', cors(corsOptions)); // Enable preflight across all routes
 
 // Error handling middleware
 app.use((err, req, res, next) => {

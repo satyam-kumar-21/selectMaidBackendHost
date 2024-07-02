af07694eee626aac601c4ab75f4706269c2ee6a5
Satyam kumar <satyamkumar2716@gmail.com>
2024-07-02 14:32:21 +0530

    c

--- index.js
+++ index.js
@@ -25,25 +25,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Allow requests from specified origins
-const allowedOrigins = [
-  'http://127.0.0.1:3000', // Add other origins as needed for different environments
-  'https://selectmaid.netlify.app/*' // Add your production domain
-];
+// const allowedOrigins = [
+//   'http://127.0.0.1:3000', // Add other origins as needed for different environments
+//   'https://selectmaid.netlify.app/*' // Add your production domain
+// ];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// };
+
+//app.use(cors(corsOptions)); // Use cors middleware with options
+
+app.use(cors({
+  origin: 'https://selectmaid.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-app.use(cors(corsOptions)); // Use cors middleware with options
+  credentials: true, // enable passing of cookies from client to server
+}));
 
 // Cloudinary setup
 const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;

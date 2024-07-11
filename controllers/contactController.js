fdf57dff12e48e63552cb23f70d585ac0fde90c8
Satyam kumar <satyamkumar2716@gmail.com>
2024-07-11 11:09:48 +0530

    change

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,16 @@
 // controllers/contact.js
 
 const nodemailer = require('nodemailer');
+const EMAIL = process.env.CONTACT_EMAIL;
+const EMAIL_SECRET = process.env.EMAIL_SECRET;
+const SELECT_MAID_EMAIL = process.env.SELECT_MAID_EMAIL; // Additional recipient email
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'satyamisha238@gmail.com', // Replace with your Gmail email
-    pass: 'hwse avgq rcsc rqtu', // Replace with your Gmail password or app-specific password
+    user: EMAIL, // Replace with your Gmail email
+    pass: EMAIL_SECRET, // Replace with your Gmail password or app-specific password
   },
- 
 });
 
 const submitMessage = async (req, res) => {
@@ -17,8 +19,8 @@ const submitMessage = async (req, res) => {
   // Create email content
   const mailOptions = {
     from: email,
-    to: 'satyamisha238@gmail.com', // Replace with recipient's email
-    subject: 'New Message from Contact Form',
+    to: `${EMAIL}, ${SELECT_MAID_EMAIL}`, // Include both recipients here
+    subject: 'New Message from Contact Form of selectmaid.in',
     html: `<p><strong>Name:</strong> ${name}</p>
            <p><strong>Email:</strong> ${email}</p>
            <p><strong>Message:</strong> ${message}</p>`,

c7726a41e64141473275f431256e10bd6d83a325
Satyam kumar <satyamkumar2716@gmail.com>
2024-07-17 20:33:43 +0530

    change

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -5,7 +5,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 const EMAIL = process.env.CONTACT_EMAIL;
 const EMAIL_SECRET = process.env.EMAIL_SECRET;
-const SELECT_MAID_EMAIL = process.env.SELECT_MAID_EMAIL; // Additional recipient email
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -20,8 +19,8 @@ const submitMessage = async (req, res) => {
 
   // Create email content
   const mailOptions = {
-    from: email,
-    to: EMAIL, // Include both recipients here
+    from: email, 
+    to: `${EMAIL}`, // Your email(s)
     subject: `New Message from Contact Form of selectmaid.in`,
     html: `<p><strong>Name:</strong> ${name}</p>
            <p><strong>Email:</strong> ${email}</p>
@@ -39,6 +38,7 @@ const submitMessage = async (req, res) => {
     res.status(500).json({ message: 'Failed to send message' });
   }
 };
+
 
 module.exports = {
   submitMessage,
